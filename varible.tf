ed39368de07f1db2d5a6405716d2e381a8accc79
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2024-10-18 15:24:06 -0700

    Update varible.tf

--- varible.tf
+++ varible.tf
@@ -1,30 +1,21 @@
-variable "node_group_name"{
-    type = string
-    description = "This variabele defines name of the node-group"
-    default = ""
+variable "node_group_name" {
+  default = "sandbox-node-group"
 }
 
-variable "eks_node_role"{
-    type = string
-    description = "This variabele defines name of the node-role"
-    default = ""
+variable "eks_node_role" {
+  default = "airbyte-sandbox-role"
 }
 
-variable "eks_cluster_role"{
-    type = string
-    description = "This variabele defines name of the cluster-role"
-    default = ""
+variable "eks_cluster_role" {
+  default = "airbyte-role"
 }
 
-variable "eks_cluster"{
-    type = string
-    description = "This variabele defineseks cluster name"
-    default = ""
+variable "eks_cluster" {
+  default = "k8s-airbyte-sandbox"
 }
 
-variable "cidr_block"{
-    description = "This variabele defines cidr_block"
-    default = [""]
+variable "cidr_block" {
+  default = "10.0.0.0/16"
 }
 
 
