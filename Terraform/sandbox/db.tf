8023e2ee64d6da446d7edc01c459ab89b0513cee
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2025-06-13 10:21:14 -0700

    Update db.tf

--- Terraform/sandbox/db.tf
+++ Terraform/sandbox/db.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "sandbox_db_instance" {
   identifier              = "sandbox-airbyte-db"
   storage_encrypted       = true
   engine                  = "postgres"
-  engine_version          = "15.5"
+  engine_version          = "17.4"
   instance_class          = "db.t3.micro"
   allocated_storage       = 20
   storage_type            = "gp2"
